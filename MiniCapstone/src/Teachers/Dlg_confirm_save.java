d8bf925ce5d0a77197e711bd8eb68ca6f9102d9c
Cordura <Cordura@192.168.1.58>
2016-02-08 11:39:48 -0800

    cordura commit 1

---
+++ MiniCapstone/src/Teachers/Dlg_confirm_save.java
@@ -0,0 +1,304 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Teachers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import mijzcx.synapse.desk.utils.CloseDialog;
+import mijzcx.synapse.desk.utils.KeyMapping;
+import mijzcx.synapse.desk.utils.KeyMapping.KeyAction;
+
+
+
+
+
+/**
+ *
+ * @author Cordura
+ */
+public class Dlg_confirm_save extends javax.swing.JDialog {
+
+    /** Creates new form Dlg_confirm_save */
+    //<editor-fold defaultstate="collapsed" desc=" callback ">
+    private Callback callback;
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+
+
+}
+
+    public static interface Callback {
+
+    void ok(CloseDialog closeDialog, OutputData data);
+}
+
+public static class InputData {
+}
+
+public static class OutputData {
+}
+//</editor-fold>
+ 
+    //<editor-fold defaultstate="collapsed" desc=" Constructors ">
+private Dlg_confirm_save(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        setUndecorated(true);
+        initComponents();
+        myInit();
+    }
+
+    private Dlg_confirm_save(java.awt.Dialog parent, boolean modal) {
+        super(parent, modal);
+        setUndecorated(true);    
+        initComponents();
+        myInit();
+    }
+
+    public Dlg_confirm_save() {
+        super();
+        setUndecorated(true);
+        initComponents();
+        myInit();
+
+    }
+    private Dlg_confirm_save myRef;
+
+    private void setThisRef(Dlg_confirm_save myRef) {
+        this.myRef = myRef;
+    }
+    private static java.util.Map<Object, Dlg_confirm_save> dialogContainer = new java.util.HashMap();
+
+    public static void clearUpFirst(java.awt.Window parent) {
+        if (dialogContainer.containsKey(parent)) {
+            dialogContainer.remove(parent);
+        }
+    }
+
+    public static Dlg_confirm_save create(java.awt.Window parent, boolean modal) {
+
+        if (modal) {
+            return create(parent, ModalityType.APPLICATION_MODAL);
+        }
+
+        return create(parent, ModalityType.MODELESS);
+
+    }
+
+    public static Dlg_confirm_save create(java.awt.Window parent, java.awt.Dialog.ModalityType modalType) {
+
+        if (parent instanceof java.awt.Frame) {
+
+            Dlg_confirm_save dialog = dialogContainer.get(parent);
+
+            if (dialog == null) {
+                dialog = new Dlg_confirm_save((java.awt.Frame) parent, false);
+                dialog.setModalityType(modalType);
+                dialogContainer.put(parent, dialog);
+                java.util.logging.Logger.getAnonymousLogger().log(Level.INFO, "instances: {0}", dialogContainer.size());
+                dialog.setThisRef(dialog);
+                return dialog;
+            } else {
+                dialog.setModalityType(modalType);
+                return dialog;
+            }
+
+        }
+
+        if (parent instanceof java.awt.Dialog) {
+            Dlg_confirm_save dialog = dialogContainer.get(parent);
+
+            if (dialog == null) {
+                dialog = new Dlg_confirm_save((java.awt.Dialog) parent, false);
+                dialog.setModalityType(modalType);
+                dialogContainer.put(parent, dialog);
+                java.util.logging.Logger.getAnonymousLogger().log(Level.INFO, "instances: {0}", dialogContainer.size());
+                dialog.setThisRef(dialog);
+                return dialog;
+            } else {
+                dialog.setModalityType(modalType);
+                return dialog;
+            }
+
+        }
+
+        return null;
+
+    }
+    //</editor-fold>    
+
+    //<editor-fold defaultstate="collapsed" desc=" main ">
+    public static void main(String args[]) {
+
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        Dlg_confirm_save dialog = Dlg_confirm_save.create(new javax.swing.JFrame(), true);
+        dialog.setVisible(true);
+
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc=" added ">
+    @Override
+        public void setVisible(boolean visible) {
+        super.setVisible(visible);
+        if (visible == true) {
+            getContentPane().removeAll();
+            initComponents();
+            myInit();
+            repaint();
+        }
+
+
+    }
+
+    public javax.swing.JPanel getSurface() {
+        return (javax.swing.JPanel) getContentPane();
+    }
+
+    public void nullify() {
+        myRef.setVisible(false);
+        myRef = null;
+    }
+    //</editor-fold>
+
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setText("                Are you sure you want to save?");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton1.setText("NO");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton2.setText("YES");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(45, 45, 45)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(115, 115, 115))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        disposed();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+         ok();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+   
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+
+    private void myInit() {
+        init_key();
+    }
+
+    public void do_pass(){
+ 
+    }
+    // <editor-fold defaultstate="collapsed" desc="Key">
+    private void disposed() {
+        this.dispose();
+    }
+
+    private void init_key() {
+        KeyMapping.mapKeyWIFW(getSurface(),
+                KeyEvent.VK_ESCAPE, new KeyAction() {
+            
+            @Override
+        public void actionPerformed(ActionEvent e) {
+//                btn_0.doClick();
+                disposed();
+            }
+        });
+    }
+    // </editor-fold> private void disposed() {
+    
+  
+    private void ok() {
+        if (callback != null) {
+            callback.ok(new CloseDialog(this), new Dlg_confirm_save.OutputData());
+        }
+    }
+    
+    
+}
---
+++ MiniCapstone/src/Teachers/MyConnection.java
@@ -0,0 +1,180 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Teachers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author i1
+ */
+public class MyConnection {
+
+    private MyConnection() {
+    }
+    private static MyConnection connection;
+
+    private static void init() {
+        if (connection == null) {
+            connection = new MyConnection();
+        }
+    }
+    private static Connection conn;
+    static String hosts = "localhost:3306";
+
+    public static Connection connect() {
+        try {
+            //        init();
+            String host = System.getProperty("pool_host", "localhost:3306");
+            String user = System.getProperty("pool_user", "root");
+            String password = System.getProperty("pool_password", "PASSWORD");
+            String db_name = System.getProperty("mydb", "teachers");
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/" + db_name;
+
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                //DriverManager.getConnection("proxool.pool_connection");
+            } catch (SQLException ex) {
+                Logger.getLogger(MyConnection.class.getName()).
+                        log(Level.SEVERE, null, ex);
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyConnection.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static Connection connect3() {
+        try {
+            //        init();
+            String host = System.getProperty("pool_host", "localhost:3306");
+            String user = System.getProperty("pool_user", "root");
+            String password = System.getProperty("pool_password", "password");
+            String db_name = System.getProperty("mydb", "db_algorithm");
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/" + db_name;
+
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                //DriverManager.getConnection("proxool.pool_connection");
+            } catch (SQLException ex) {
+                Logger.getLogger(MyConnection.class.getName()).
+                        log(Level.SEVERE, null, ex);
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyConnection.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static Connection connect2() {
+        try {
+            //        init();
+            String host = System.getProperty("pool_host", "localhost:3306");
+            String user = System.getProperty("pool_user", "root");
+            String password = System.getProperty("pool_password", "password");
+            String db_name = System.getProperty("mydb", "db_algorithm");
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/";
+
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                //DriverManager.getConnection("proxool.pool_connection");
+            } catch (SQLException ex) {
+                Logger.getLogger(MyConnection.class.getName()).
+                        log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyConnection.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+
+                conn = null;
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    public static Connection cloud_connect() {
+        try {
+            //        init();
+            String host = System.getProperty("pool_host1", "128.199.80.53:3306");
+            String user = System.getProperty("pool_user1", "root");
+            String password = System.getProperty("pool_password1", "password");
+            String db_name = System.getProperty("mydb1", "db_algorithm");
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/" + db_name;
+
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                //DriverManager.getConnection("proxool.pool_connection");
+            } catch (SQLException ex) {
+                Logger.getLogger(MyConnection.class.getName()).
+                        log(Level.SEVERE, null, ex);
+            }
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyConnection.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        int connected = check_cloud_connection();
+        System.out.println("Is Connected: " + connected);
+    }
+
+    public static int check_cloud_connection() {
+        int connected = 0;
+
+        String host = System.getProperty("pool_host1", "128.199.80.53:3306");
+        String user = System.getProperty("pool_user1", "root");
+        String password = System.getProperty("pool_password1", "password");
+        String db_name = System.getProperty("mydb1", "db_algorithm");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/" + db_name;
+            try {
+                Connection conn1 = DriverManager.getConnection(url, user, password);
+                String s0 = "select "
+                        + " id"
+                        + " from users"
+                        + " limit 1";
+                Statement stmt = conn1.createStatement();
+                ResultSet rs = stmt.executeQuery(s0);
+                if (rs.next()) {
+                    connected = 1;
+                }
+            } catch (SQLException ex) {
+                return 0;
+            }
+        } catch (ClassNotFoundException ex) {
+            return 0;
+        }
+        return connected;
+
+    }
+
+}
